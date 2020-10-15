546d88d4cda58c180b6e17f5978d286145c60942
jms90 <justom78@gmail.com>
2020-10-15 20:21:15 +0200

    fix and create seed

--- database/migrations/2020_10_15_174442_create_sorteos_table.php
+++ database/migrations/2020_10_15_174442_create_sorteos_table.php
@@ -19,7 +19,7 @@ class CreateSorteosTable extends Migration
             $table->string('descripcion');
             $table->date('fecha_fin');
             $table->bigInteger('desarrolladora_id')->unsigned();
-            $table->timestaps();
+            $table->timestamps();
         });
     }
 
