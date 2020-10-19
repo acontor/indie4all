3a871fe6db079f764891f4d2a959230bd59c77c5
Álvaro Conde Torres <alvarocndtorres@gmail.com>
2020-10-19 23:30:16 +0200

    Arreglos en controlador y rutas.

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
  * Admin Routes
  */
 
- // Logros
+// Logros
 Route::get('/admin/logros', [App\Http\Controllers\Administrador\LogrosController::class, 'index'])->name('logros.index');
 Route::post('/admin/logros', [App\Http\Controllers\Administrador\LogrosController::class, 'store'])->name('logros.store');
-Route::get('/admin/logros/{id}', [App\Http\Controllers\Administrador\LogrosController::class, 'show'])->name('logros.show');
-Route::get('/admin/logros/{id}/edit', [App\Http\Controllers\Administrador\LogrosController::class, 'edit'])->name('logros.edit');
 Route::post('/admin/logros/{id}/update', [App\Http\Controllers\Administrador\LogrosController::class, 'update'])->name('logros.update');
 Route::get('/admin/logros/{id}/delete', [App\Http\Controllers\Administrador\LogrosController::class, 'destroy'])->name('logros.destroy');
