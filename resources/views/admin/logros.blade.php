8f06cb807712db1e124797f0c60893f3f7ecb12d
Álvaro Conde Torres <alvarocndtorres@gmail.com>
2020-10-22 00:13:33 +0200

    Mejoras visuales

--- resources/views/admin/logros.blade.php
+++ resources/views/admin/logros.blade.php
@@ -3,9 +3,7 @@
     <div class="container">
         <div class='row'>
             <div class='col-sm'>
-
                 <h1 class='display-5'>Logros <a class='btn btn-success button-crear'>+</a></h1>
-
                 @if ($errors->any())
                     <div class='alert alert-danger'>
                         <ul>
@@ -21,7 +19,6 @@
                         {{ session()->get('success') }}
                     </div>
                 @endif
-
                 <div class="d-none form-crear">
                     <form method='post' action="{{ route('admin.logros.store') }}">
                         @csrf
@@ -41,51 +38,48 @@
                         <button type='submit' class='btn btn-success mb-3'>Añadir</button>
                     </form>
                 </div>
-
-                <table class='table table-striped table-responsive-sm'>
-                    <thead>
-                        <tr>
-                            <td>Nombre</td>
-                            <td>Descripción</td>
-                            <td>Icono</td>
-                            <td>Editar</td>
-                            <td>Borrar</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($logros as $logro)
-
+                <div class="table-responsive">
+                    <table class='table table-striped table-responsive'>
+                        <thead>
                             <tr>
-                                <form action="{{ route('admin.logros.update', $logro->id) }}" method='post'>
-                                    @csrf
-                                    @method('PATCH')
-                                    <td class="align-middle"><input type="text" placeholder="Nombre"
-                                            value="{{ $logro->nombre }}" name="nombre"></td>
-                                    <td class="align-middle"><textarea id="descripcion" class="align-middle"
-                                            placeholder="Descripción" name="descripcion" rows="1"
-                                            cols="40">{{ $logro->descripcion }}</textarea></td>
-                                    <td class="align-middle"><input type='text' placeholder="Icono"
-                                            value="{{ $logro->icono }}" name="icono">
-                                    </td>
-                                    <td class="align-middle"><button type="submit" class='btn btn-primary'>Editar</button>
-                                    </td>
-                                </form>
-
-                                <td>
-
-                                    <form action="{{ route('admin.logros.destroy', $logro->id) }}" method='post'>
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class='btn btn-danger' type='submit'>Borrar</button>
-                                    </form>
-
-                                </td>
-
+                                <td>Nombre</td>
+                                <td>Descripción</td>
+                                <td>Icono</td>
+                                <td>Editar</td>
+                                <td>Borrar</td>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $logros->links('pagination::bootstrap-4') }}
+                        </thead>
+                        <tbody>
+                            @foreach ($logros as $logro)
+                                <tr>
+                                    <form action="{{ route('admin.logros.update', $logro->id) }}" method='post'>
+                                        @csrf
+                                        @method('PATCH')
+                                        <td class="align-middle"><input type="text" placeholder="Nombre"
+                                                value="{{ $logro->nombre }}" name="nombre"></td>
+                                        <td class="align-middle"><textarea id="descripcion" class="align-middle"
+                                                placeholder="Descripción" name="descripcion" rows="1"
+                                                cols="40">{{ $logro->descripcion }}</textarea></td>
+                                        <td class="align-middle"><input type='text' placeholder="Icono"
+                                                value="{{ $logro->icono }}" name="icono">
+                                        </td>
+                                        <td class="align-middle"><button type="submit"
+                                                class='btn btn-primary'>Editar</button>
+                                        </td>
+                                    </form>
+                                    <td>
+                                        <form action="{{ route('admin.logros.destroy', $logro->id) }}" method='post'>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class='btn btn-danger' type='submit'>Borrar</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $logros->links('pagination::bootstrap-4') }}
+                </div>
             </div>
         </div>
     </div>
@@ -95,12 +89,13 @@
             $(".button-crear").click(function() {
                 $(".form-crear").toggleClass("d-none");
                 $(this).toggleClass("btn-danger");
-                if($(this).hasClass("btn-danger")) {
+                if ($(this).hasClass("btn-danger")) {
                     $(this).text("-");
                 } else {
                     $(this).text("+");
                 }
-            })
-        })
+            });
+        });
+
     </script>
 @endsection
