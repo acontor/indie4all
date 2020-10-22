c2a42d34d614dbc3536f795cc10abec8a5cdd679
Álvaro Conde Torres <alvarocndtorres@gmail.com>
2020-10-22 22:33:28 +0200

    Sidebar y nav main menu parcializados en plantilla base del panel de administración

---
+++ resources/views/layouts/admin/nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand navbar-dark">
+    <div class="container">
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false" v-pre>
+                    <i class="fas fa-user h3"></i>{{-- {{ Auth::user()->name }}
+                    --}}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Cerrar sesión
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
