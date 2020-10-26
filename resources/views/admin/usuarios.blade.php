0f8913592445eef2e713194b16dfcf1da464b702
Álvaro Conde Torres <alvarocndtorres@gmail.com>
2020-10-26 17:01:52 +0100

    Añadido chart tipos de usuario

--- resources/views/admin/usuarios.blade.php
+++ resources/views/admin/usuarios.blade.php
@@ -4,7 +4,8 @@
         <div class='row'>
             <div class='col-sm'>
                 <h1 class='display-5'>Usuarios ({{ $usuarios->count() }})</h1>
-                <div class="table-responsive">
+                <canvas id="myChart" width="400" height="100"></canvas>
+                <div class="table-responsive mt-3">
                     <div class="table-responsive">
                         <table class="table table-striped">
                             <thead>
@@ -48,3 +49,35 @@
         </div>
     </div>
 @endsection
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
+<script>
+    $(function() {
+
+        var masters = {!! $num_masters !!};
+        var fans = {!! $num_fans !!};
+        var cms = {!! $num_cms !!};
+
+        graficaUsuarios(masters, fans, cms)
+    });
+
+    function graficaUsuarios(masters, fans, cms) {
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var data = {
+            datasets: [{
+                backgroundColor: ['#ff6384', '#A086BE', '#333333'],
+                data: [masters, fans, cms]
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: ['Masters', 'Fans', 'Cms']
+        };
+        var myBarChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: data,
+            options: {
+            }
+        });
+    }
+
+</script>
