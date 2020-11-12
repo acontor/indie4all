48ce52f00645f2d2569bfa82220bb05c377c4a51
Álvaro Conde Torres <alvarocndtorres@gmail.com>
2020-11-12 18:48:20 +0100

    Pequeños cambios

--- app/Http/Controllers/Administrador/DesarrolladorasController.php
+++ app/Http/Controllers/Administrador/DesarrolladorasController.php
@@ -15,6 +15,7 @@ class DesarrolladorasController extends Controller
      */
     public function __construct()
     {
+        $this->middleware('auth');
         $this->middleware('admin');
     }
 
--- app/Http/Controllers/Administrador/SolicitudesController.php
+++ app/Http/Controllers/Administrador/SolicitudesController.php
@@ -17,6 +17,7 @@ class SolicitudesController extends Controller
      */
     public function __construct()
     {
+        $this->middleware('auth');
         $this->middleware('admin');
     }
 
