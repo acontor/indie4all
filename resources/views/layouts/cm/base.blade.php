42165a42ad4360f317f35178816175b166ef3942
Álvaro Conde Torres <alvarocndtorres@gmail.com>
2020-11-12 18:57:06 +0100

    Creados layouts del panel de cm

---
+++ resources/views/layouts/cm/base.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/cm.css') }}" rel="stylesheet">
+    @yield('styles')
+</head>
+
+<body>
+    <div id="app">
+        @include('layouts.cm.nav')
+
+        @include('layouts.cm.sidebar')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    @yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/cm/sidebar.blade.php
@@ -0,0 +1,10 @@
+<nav class="sidebar">
+    <ul>
+        <li><a><i class="fas fa-home"></i>Inicio</a></li>
+        <li><a><i class="fas fa-building"></i>Desarrolladora</a></li>
+        <li><a><i class="fas fa-gamepad"></i>Juegos</a></li>
+        <li><a><i class="fas fa-bullhorn"></i>Campañas</a></li>
+        <li><a><i class="fas fa-gift"></i>Sorteos</a></li>
+        <li><a><i class="fas fa-poll"></i>Encuestas</a></li>
+    </ul>
+</nav>
