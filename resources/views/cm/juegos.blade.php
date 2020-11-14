e5691834e99f44b068c9661b9fb361d91e46c993
jms90 <justom78@gmail.com>
2020-11-14 19:11:32 +0100

    metodo create/store/edit/update juego, añadidas rutas y vista juegos y juegos_create

--- resources/views/cm/juegos.blade.php
+++ resources/views/cm/juegos.blade.php
@@ -1,12 +1,165 @@
 @extends('layouts.cm.base')
 @section('content')
+    <style>
+        .mt-5 {
+            margin-top: 5rem;
+        }
+
+        .our-games {
+            background-color: #f8f9fa;
+            padding: 100px 0;
+        }
+
+        .container {
+            max-width: 960px;
+            margin: 0 auto;
+        }
+
+        .row {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+        }
+
+        .text-center {
+            text-align: center;
+            font-size: 18px;
+        }
+
+        .text-center h2 {
+            font-weight: 500;
+            font-size: 1.6em;
+        }
+
+        .text-center h2 span {
+            font-weight: 800;
+        }
+
+        .text-center p {
+            max-width: 500px;
+            margin: 0 auto;
+            color: #6c757d;
+            line-height: 1.5;
+            font-size: 1em;
+        }
+
+        .col {
+            width: 33.3333333%;
+            overflow: hidden;
+            padding: 15px;
+        }
+
+        .col .card {
+            width: 100%;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+            transition: transform 0.3s ease;
+        }
+
+        .col img {
+            width: 100%;
+            height: auto;
+            border: 0;
+        }
+
+        .col .card-body {
+            padding: 0 15px 15px 15px;
+            background-color: #fff;
+            border-bottom-left-radius: 15px;
+            border-bottom-right-radius: 15px;
+        }
+
+        .card-body h5 {
+            font-size: 24px;
+            margin: 7px 0;
+        }
+
+        .card:hover {
+            transform: translateY(-10px);
+            transition: transform 0.3s ease;
+        }
+
+        .card p {
+            color: #6c757d;
+        }
+
+        .card-body .date-lanzamiento span {
+            color: #e74c3c;
+        }
+
+        .card-body h5 a {
+            color: #222;
+        }
+
+        @media(min-width: 577px) and (max-width: 768px) {
+            .col {
+                width: 50%;
+            }
+        }
+
+        @media(max-width: 576px) {
+            .col {
+                width: 100%;
+            }
+        }
+
+    </style>
+
     <div class="container">
         <div class='row'>
             <div class='col-sm'>
                 <div class="box-header">
-                    <h1>Juegos {{ $juegos->count() }}</h1>
+                    <h1 class='d-inline-block'>Juegos {{ $juegos->count() }}</h1>
+                    <a href="{{ route('cm.juegos.create') }}" class='btn btn-success btn-sm round float-right mt-2'><i
+                            class="far fa-plus-square"></i></a>
                 </div>
             </div>
         </div>
+        <div class="row">
+            <section>
+                <div class="container">
+
+                    <div class="row mt-5">
+                        @foreach ($juegos as $juego)
+                            <div class="col">
+                                <div class="card">
+
+                                    <img src="{{ asset('/images/default.png') }}" />
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="{{ route('usuario.juego.show', $juego->id) }}">{{ $juego->nombre }}</a>
+
+                                        </h5>
+                                        <p class="date-lanzamiento">
+                                            {{ $juego->fecha_lanzamiento }} <span
+                                                class="genero">{{ $juego->genero->nombre }}</span>
+                                        </p>
+                                        <p class="card-text">
+                                            {{ $juego->sinopsis }}
+                                        </p>
+                                        <div class="row">
+                                            <a class="btn btn-primary btn-sm round ml-1" title="Editar Juego"
+                                                href="{{ route('cm.juegos.edit', $juego->id) }}">
+                                                <i class="fa fa-edit"></i>
+                                            </a>
+                                            <form action="{{ route('cm.juegos.destroy', $juego->id) }}" method='post'>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class='btn btn-danger btn-sm round ml-1' type='submit'><i
+                                                        class="far fa-trash-alt"></i></button>
+                                            </form>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </section>
+        </div>
+        {{ $juegos->links('pagination::bootstrap-4') }}
     </div>
+    </div>
+
 @endsection
---
+++ resources/views/cm/juegos_crear.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.admin.base')
+@section('content')
+    <div class="container">
+        <div class='row'>
+            <div class='col-sm'>
+                @if ($errors->any())
+                    <div class='alert alert-danger'>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <br />
+                @endif
+                <div class="box-header">
+                    @if (isset($juego))
+                        <h1 class='display-5'>Editar Juego</h1>
+                    @else
+                        <h1 class='display-5'>Nuevo Juego</h1>
+                    @endif
+
+                </div>
+                <form data-ui-jp="parsley" @if (isset($juego))
+                action="{{ route('cm.juegos.update', $juego->id) }}" @else action="{{ route('cm.juegos.store') }}"
+                    @endif method="post">
+                    @if (isset($juego)) @method('PATCH')
+                    @else @method('POST') @endif
+                    @csrf
+                    <div class="box">
+                        <div class='form-group'>
+                            <label for='nombre'>Título:</label>
+                            <input type='text' class='form-control' name='nombre' @if (isset($juego)) value="{{ $juego->nombre }}" @endif/>
+                        </div>
+                        <div class='form-group'>
+                            <label for='sinopsis'>sinopsis:</label>
+                            <textarea class=" form-control"
+                                name="sinopsis"> @if (isset($juego)){{ $juego->sinopsis }}@endif</textarea>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="form-group">
+                            <label for='fecha_lanzamiento'>Fecha de lanzamiento:</label>
+                            <input type="date" name="fecha_lanzamiento" @if (isset($juego))
+                            value="{{ $juego->fecha_lanzamiento }}" @endif>
+
+                            <label for='precio'>Precio:</label>
+                            <input type="text" name="precio" @if (isset($juego))
+                            value="{{ $juego->precio }}" @endif>
+                        </div>
+                    </div>
+                    <div class="box mt-3">
+                        <div class="row">
+                            <div class="form-group  col-sm-5">
+                                <label for='genero_id'>Género</label>
+                                <select id="genero_id" class="form-control select2" data-ui-jp="select2"
+                                    data-ui-options="{theme: 'bootstrap'}" name="genero_id">
+                                    <option value="">Género</option>
+                                    @foreach ($generos as $genero)
+                                        <option value="{{ $genero->id }}" @if (isset($juego))
+                                            @if ($juego->genero->id == $genero->id)
+                                                selected @endif
+                                    @endif>{{ $genero->nombre }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+
+                            <div class='form-group'>
+                                <span class="btn btn-file">
+                                    <i class="fas fa-upload"></i>Portada:
+                                    <input type='file' class='inputfile' name='imagen_portada' id='imagen_portada'
+                                        @if (isset($juego))
+                                    value="{{ $juego->imagen_portada }}" @endif/>
+                                </span>
+                            </div>
+                            <div class='form-group'>
+                                <span class="btn btn-file">
+                                    <i class="fas fa-upload"></i> Carátula:
+                                    <input type='file' class='inputfile' name='imagen_caratula' id='imagen_caratula'
+                                        @if (isset($juego))
+                                    value="{{ $juego->imagen_caratula }}" @endif />
+                                </span>
+                            </div>
+                        </div>
+                        <button type='submit' class='btn btn-success mb-3'>Crear</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/i18n/es.js"></script>
+    <script>
+        $('#genero').select2({
+            language: 'es',
+            width: '100%',
+            placeholder: 'generos...',
+            allowClear: true,
+            dropdownAutoWidth: true
+        });
+
+    </script>
+@endsection
