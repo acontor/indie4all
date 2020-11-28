a3d807e23c515acdaaf2beaf4f9a0693abc36146
jms90 <justom78@gmail.com>
2020-11-29 00:22:42 +0100

    registro/login diseño pagina juegos

--- resources/views/usuario/juegos.blade.php
+++ resources/views/usuario/juegos.blade.php
@@ -1,45 +1,49 @@
 @extends("layouts.usuario.base")
 @section("styles")
     <link href="{{ asset('css/cm.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/juegos.css') }}" rel="stylesheet">   
 @endsection
 @section("content")
     <div class="container">
         <div class="row">
             <div class="col-sm">
                 <div class="box-header">
-                    <h1 class="d-inline-block">Juegos ({{ $juegos->count() }})</h1>
-                   
-                </div>
+                    <h1 class="d-inline-block">Juegos ({{ $juegos->count() }})</h1>                   
+                </div>                
             </div>
         </div>
-        <div class="row">
-            <section>
-                <div class="container">
-                    <div class="row">
-                        @foreach ($juegos as $juego)
-                            <div class="mt-4 mr-4">
-                                <div class="card">
-                                    <img src="{{url('/images/juegos/portadas/'.$juego->imagen_portada)}}"width="250" height="200" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            <a href="{{ route('usuario.juego.show', $juego->id) }}">{{ $juego->nombre }}</a>
-                                        </h5>
-                                        <p class="date-lanzamiento"> {{ $juego->fecha_lanzamiento }}
-                                            <span class="genero">{{ $juego->genero->nombre }}</span>
-                                        </p>
-                                        <p class="card-text">
-                                            {{ $juego->sinopsis }}
-                                        </p>
-                                        <div class="row">
-                                     {{-- Botonos de follow/unfollow notificaciones --}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
+        <div class="cd-slider">
+            <ul>
+                @foreach ($juegos as $juego)
+                <li>
+                    <div class="image" style="background-image:url({{url('/images/juegos/portadas/'.$juego->imagen_portada)}});"></div>
+                    <div class="content">
+                    <h2>{{$juego->nombre}}</h2>
+                    <a href="{{ route('usuario.juego.show', $juego->id) }}">Ver Juego</a>  <br>
+                    <a href="#">{{$juego->genero->nombre}}</a>    
+                    <p> {{$juego->sinopsis}}</p>                
+                
                     </div>
+                </li>
+                @endforeach
+                    
+            </ul>
+        </div> <!--/.cd-slider-->            
+            <div class="row mt-3">
+                <div class="col-8 noticias">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae quas animi cum exercitationem mollitia fugiat, est assumenda dolor nihil molestias, ab veritatis amet quis blanditiis debitis officia. Quis, sed numquam!
                 </div>
-            </section>
+                <div class="col-4">
+                    <div class="row recomendados">
+                        5 juegos con los géneros seguidos y más seguidores
+                    </div>
+                    <div class="row seguidos">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A aliquid iste, ad, est vel doloribus repudiandae earum deleniti rem laudantium officiis numquam facilis recusandae, ex unde assumenda libero voluptas sint.</div>
+                </div>
+            </div>        
         </div>
     </div>
+@section('scripts')
+<script src="{{ asset('js/juegos.js') }}"></script>
+@endsection
+
 @endsection
